479acc22f5e756dbd7ee1dfc3c72c26a842c8874
duy0299 <baoduy0299@gmail.com>
2022-10-17 09:47:29 +0700

    create method findByEmail

--- src/main/java/com/rookies/assignment/data/repository/IUserInfoRepository.java
+++ src/main/java/com/rookies/assignment/data/repository/IUserInfoRepository.java
@@ -3,6 +3,7 @@ package com.rookies.assignment.data.repository;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.rookies.assignment.data.entity.UserInfo;
@@ -10,4 +11,8 @@ import com.rookies.assignment.data.entity.UserInfo;
 @Repository
 public interface IUserInfoRepository extends JpaRepository<UserInfo, UUID>{
 
+
+    public UserInfo findByEmailAndPassword(String email, String pass);
+
+    public UserInfo findByEmail(String email);
 }
