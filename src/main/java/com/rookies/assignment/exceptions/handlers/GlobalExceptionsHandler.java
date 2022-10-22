c98233f5a500df1ecc285c13068124e843df42c9
duy0299 <baoduy0299@gmail.com>
2022-10-22 16:30:09 +0700

    modified exception

--- src/main/java/com/rookies/assignment/exceptions/handlers/GlobalExceptionsHandler.java
+++ src/main/java/com/rookies/assignment/exceptions/handlers/GlobalExceptionsHandler.java
@@ -1,4 +1,4 @@
-package com.rookies.assignment.controller.handlers;
+package com.rookies.assignment.exceptions.handlers;
 
 import com.rookies.assignment.dto.response.ResponseDto;
 import com.rookies.assignment.exceptions.*;
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import javax.validation.constraints.Null;
+
 @ControllerAdvice
 public class GlobalExceptionsHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler({ResourceFoundException.class})
@@ -18,7 +20,13 @@ public class GlobalExceptionsHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<ResponseDto>(error, HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler({IllegalArgumentException.class, RepeatDataException.class})
+    @ExceptionHandler({ RepeatDataException.class})
+    protected ResponseEntity handleRepeatDataException(RuntimeException exception) {
+        ResponseDto error = new ResponseDto("400", exception.getMessage(), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<ResponseDto>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler({IllegalArgumentException.class})
     protected ResponseEntity handleIllegalArgumentException(RuntimeException exception) {
         ResponseDto error = new ResponseDto("400", exception.getMessage(), HttpStatus.BAD_REQUEST);
         return new ResponseEntity<ResponseDto>(error, HttpStatus.BAD_REQUEST);
@@ -27,8 +35,8 @@ public class GlobalExceptionsHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler({Exception.class})
     protected ResponseEntity handleException(RuntimeException exception,
                                           WebRequest request) {
-        ResponseDto error = new ResponseDto("500", "Unknow error", null);
-        return new ResponseEntity<ResponseDto>(error, HttpStatus.NOT_FOUND);
+        ResponseDto error = new ResponseDto("500", exception.getMessage(), HttpStatus.valueOf(500));
+        return new ResponseEntity<ResponseDto>(error, HttpStatus.valueOf(500));
     }
 
     @ExceptionHandler({ParamNotValidException.class})
@@ -58,6 +66,17 @@ public class GlobalExceptionsHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<ResponseDto>(error, HttpStatus.METHOD_NOT_ALLOWED);
     }
 
+    @ExceptionHandler({ForbiddenException.class})
+    protected ResponseEntity handleForbiddenException(RuntimeException exception) {
+        ResponseDto error = new ResponseDto("403", exception.getMessage(), HttpStatus.FORBIDDEN);
+        return new ResponseEntity<ResponseDto>(error, HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler({NullPointerException.class})
+    protected ResponseEntity NullPointerExceptionException(RuntimeException exception) {
+        ResponseDto error = new ResponseDto("498", exception.getMessage(), "NullPointerException");
+        return new ResponseEntity<ResponseDto>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
     public GlobalExceptionsHandler() {
         super();
